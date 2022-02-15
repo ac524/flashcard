ce5414734214927855341cf5ea7bc25d10b43c3e
Anthony Brown <ac524.brown@gmail.com>
2022-02-14 18:08:28 -0800

    Removing activity model references for Project.

--- models/index.js
+++ models/index.js
@@ -1,13 +1,12 @@
 const User = require('./User');
-const Project = require('./Project');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
+// User.hasMany(Project, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+// Project.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
 
-module.exports = { User, Project };
+module.exports = { User };
